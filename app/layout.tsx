fe087a14de03819a19caf035f100af2b6cb43f37
itsaljunaid <153025228+itsaljunaid@users.noreply.github.com>
2024-05-10 08:50:44 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Liftoff - AI-Powered Mock Interviews",
   openGraph: {
-    title: "Liftoff - AI-Powered Mock Interviews",
+    title: "AI Interview - An AI-Powered Interviews Training Tool",
     description:
-      "Liftoff is an AI-powered mock interview platform that helps you practice for your next job interview.",
+      "AI Interview is an AI-powered interview platform that helps you practice for your next job interview.",
     images: [
       {
         url: "https://demo.useliftoff.com/opengraph-image",
@@ -15,9 +15,9 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Liftoff - AI-Powered Mock Interviews",
+    title: "AI Interview - An AI-Powered Interviews Training Tool",
     description:
-      "Liftoff is an AI-powered mock interview platform that helps you practice for your next job interview.",
+      "AI Interview is an AI-powered interview platform that helps you practice for your next job interview.",
     images: ["https://demo.useliftoff.com/opengraph-image"],
     creator: "@tmeyer_me",
   },
